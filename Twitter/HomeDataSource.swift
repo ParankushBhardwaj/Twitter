841c8ba1a22031d68e942ef85bca5235824d6459
Parankush <parankush@me.com>
2017-03-29 17:14:31 -0400

    Finished Tweets Section

--- Twitter/HomeDataSource.swift
+++ Twitter/HomeDataSource.swift
@@ -19,7 +19,21 @@ class HomeDataSource: Datasource {
     }()
     
     
-    let tweets = ["tweet1", "tweet2"]
+    
+    
+    let tweets: [Tweet] = {
+        
+        let me = User(name: "Parankush Bhardwaj", username: "@ParankushB", bioText: "iOS Developer. Proud fanboy of Apple and Tesla. Coffee addict.", profileImage: #imageLiteral(resourceName: "profile_image"))
+        let tweet = Tweet(user: me, message: "\"Choosing ..what you know will be unpopular in the short run but ..will prove correct in the long run takes courage\" ")
+        
+        let tweet2 = Tweet(user: me, message: "As tech outgrows the tech industry, and raw feeds & speeds matter less and less, Apple plays more & more to its strengths.")
+
+        
+    
+        return [tweet, tweet2]
+    }()
+    
+    
     
 
     override func headerClasses() -> [DatasourceCell.Type]? {
@@ -35,6 +49,9 @@ class HomeDataSource: Datasource {
     }
     
     override func item(_ indexPath: IndexPath) -> Any? {
+        if indexPath.section == 1 {
+            return tweets[indexPath.item]
+        }
         return users[indexPath.item]
     }
     
---
+++ Twitter/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  Twitter
+//
+//  Created by Parankush Bhardwaj on 3/29/17.
+//  Copyright © 2017 Parankush Bhardwaj. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let user: User
+    let message: String
+}
